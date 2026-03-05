8be26b0d6d296878bb2f90a0e3bb65f0a90ffdf4
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-03-04 20:03:47 -0500

    feat: implement tag_list handler

--- crates/habiting-server/src/tag_service/tag_list.rs
+++ crates/habiting-server/src/tag_service/tag_list.rs
@@ -1,4 +1,6 @@
-use super::*;
+use sqlx::PgExecutor;
+
+use super::{tag_row::TagPgRow, *};
 
 impl HabitingTagService {
     pub(super) async fn handle_list(
@@ -8,8 +10,38 @@ impl HabitingTagService {
         let req = req.into_inner();
         info!("{req:?}");
 
-        let _conn = Config::try_get().await?.db_conn.get();
+        let conn = Config::try_get().await?.db_conn.get();
 
-        todo!()
+        let tags = tokio::time::timeout(DBConn::context(), tag_list(conn))
+            .await
+            .map_err(|_| DbError::Context("tag_list"))??
+            .into_iter()
+            .map(habiting_proto::Tag::from)
+            .collect();
+
+        Ok(Response::new(TagListResponse { tags }))
+    }
+}
+
+async fn tag_list(conn: impl PgExecutor<'_>) -> Result<Vec<TagPgRow>, DbError> {
+    Ok(sqlx::query_as!(TagPgRow, "SELECT * FROM tags")
+        .fetch_all(conn)
+        .await?)
+}
+
+#[cfg(test)]
+mod test {
+    use sqlx::PgPool;
+
+    use super::*;
+
+    #[sqlx::test(fixtures("../../fixtures/tags.sql"))]
+    async fn test_list(conn: PgPool) -> anyhow::Result<()> {
+        let desc = "Basic test, listing all items in tags table";
+        let want = 2;
+        let got = tag_list(&conn).await?;
+
+        assert_eq!(want, got.len(), "{desc}");
+        anyhow::Ok(())
     }
 }
