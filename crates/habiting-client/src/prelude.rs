741518b7fbb3f18bae55883911a95084afa66bbd
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-03-05 21:38:39 -0500

    refactor(client): change visibilty of serveral modules to crate

--- crates/habiting-client/src/prelude.rs
+++ crates/habiting-client/src/prelude.rs
@@ -1,8 +1,8 @@
-pub mod habiting_proto {
+pub(crate) mod habiting_proto {
     tonic::include_proto!("habiting");
 }
 // internal prelude exports
 pub use super::cli::prelude::*;
-pub use super::config::prelude::*;
-pub use super::errors::prelude::*;
+pub(crate) use super::config::prelude::*;
+pub(crate) use super::errors::prelude::*;
 pub use super::session_service::prelude::*;
