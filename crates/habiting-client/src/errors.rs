3d6057007cab99f632348aba91512818d8f56cc9
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-03-05 20:09:58 -0500

    feat(client): handle and format errors from server

--- crates/habiting-client/src/errors.rs
+++ crates/habiting-client/src/errors.rs
@@ -1,7 +1,9 @@
+use std::fmt::Display;
+
 use thiserror::Error;
 
 pub mod prelude {
-    pub use super::ClientError;
+    pub use super::{ClientError, ServerError};
 }
 
 #[derive(Debug, Error)]
@@ -12,4 +14,16 @@ pub enum ClientError {
     UnsetEnvVar(&'static str),
     #[error("Can't parse {1}, as type {0}")]
     InvalidConfig(&'static str, String),
+    #[error("Issue connecting to endpoint")]
+    Connection(#[from] tonic::transport::Error),
+}
+
+#[derive(Debug, Error)]
+/// Errors returned from the server
+pub struct ServerError(pub tonic::Status);
+
+impl Display for ServerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\n\n{}.\n{}.", self.0.code(), self.0.message())
+    }
 }
